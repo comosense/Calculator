91b1c6c3fb2b64d1b03b8b15e5ee4876baf0967a
CoMoSense <comosense@gmail.com>
2026-09-14 15:51:46 +0900

    update

---
+++ app/src/main/java/com/gmail/comosense/calculator/domain/Symbol.kt
@@ -0,0 +1,30 @@
+package com.gmail.comosense.calculator.domain
+
+sealed interface Symbol {
+    sealed interface Sign : Symbol {
+        data object Positive : Sign
+        data object Negative : Sign
+    }
+
+    sealed interface Numeric : Symbol {
+        data class Digit(val value: Int) : Numeric
+        data object Point : Numeric
+    }
+
+    sealed interface FactorStart : Symbol {
+        data object OpenParenthesis : FactorStart
+    }
+
+    sealed interface FactorEnd : Symbol {
+        data object CloseParenthesis : FactorEnd
+    }
+
+    sealed interface Operator : Symbol {
+        data object Add : Operator
+        data object Subtract : Operator
+        data object Multiply : Operator
+        data object Divide : Operator
+    }
+
+    data class Error(val value: String) : Symbol
+}
