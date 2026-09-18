8bad932d8db788754e8a8d306b779861d55ac194
CoMoSense <comosense@gmail.com>
2026-09-18 17:59:56 +0900

    update

--- app/src/main/java/com/gmail/comosense/calculator/presentation/SymbolFormatter.kt
+++ app/src/main/java/com/gmail/comosense/calculator/presentation/SymbolFormatter.kt
@@ -1,7 +1,10 @@
 package com.gmail.comosense.calculator.presentation
 
-import com.gmail.comosense.calculator.common.formatNumericString
 import com.gmail.comosense.calculator.domain.Symbol
+import java.math.BigInteger
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
 import java.util.Locale
 
 val Symbol.text: String
@@ -48,4 +51,50 @@ fun List<Symbol>.formatSymbols(locale: Locale): String {
         }
         flushNumeric()
     }
+}
+
+private fun formatNumericString(value: String, locale: Locale): String {
+    val pointIndex: Int = value.indexOf(".")
+    val integerPart: String = if (pointIndex >= 0) {
+        value.substring(0, pointIndex)
+    } else {
+        value
+    }
+
+    val decimalPart: String? = if (pointIndex >= 0) {
+        value.substring(pointIndex + 1)
+    } else {
+        null
+    }
+
+    val formattedIntegerPart: String = formatIntegerPart(
+        integerPart = integerPart,
+        locale = locale,
+    )
+
+    return if (decimalPart == null) {
+        formattedIntegerPart
+    } else {
+        buildString {
+            append(formattedIntegerPart)
+            append(DecimalFormatSymbols.getInstance(locale).decimalSeparator)
+            append(decimalPart)
+        }
+    }
+}
+
+private fun formatIntegerPart(integerPart: String, locale: Locale): String {
+    if (integerPart.isEmpty()) return integerPart
+
+    val formatter: NumberFormat = NumberFormat.getNumberInstance(locale)
+    if (formatter !is DecimalFormat) {
+        return integerPart
+    }
+
+    formatter.isGroupingUsed = true
+    formatter.maximumIntegerDigits = integerPart.length
+    formatter.minimumFractionDigits = 0
+    formatter.maximumFractionDigits = 0
+
+    return formatter.format(BigInteger(integerPart))
 }
